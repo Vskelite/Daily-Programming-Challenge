cfdb8db2a9d1fdd3be87de0964a8d5d67dc24f6f
Vskelite <khamkarv01@gmail.com>
2024-09-10 15:32:37 +0530

    Day2 challenge solved

--- Day2.java
+++ Day2.java
@@ -1,4 +1,13 @@
+// Problem: Find the missing number
+// You are given an array arr containing n-1 distinct integers. The array consists of integers taken from the range 1 to n, meaning one integer is missing from this sequence. Your task is to find the missing integer.
 
+// Input :
+// An integer array arr of size n-1 where the elements are distinct and taken from the range 1 to n.
+// Example : arr = [1, 2, 4, 5]
+
+// Output :
+// Return the missing integer from the array.
+// Example: Missing Number : 3
 
 public class Day2{
 
